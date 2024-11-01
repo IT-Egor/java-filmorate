408d968f0d581335b596557d874b4f823f2a5554
IT-Egor <egor.belousov.04@yandex.ru>
2024-11-01 12:55:50 +0300

    Add post and get mapping to FilmController

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,36 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/films")
 public class FilmController {
+
+    private final Map<Integer, Film> films = new HashMap<>();
+
+    @GetMapping
+    public Collection<Film> getAllFilms() {
+        return films.values();
+    }
+
+    @PostMapping
+    public Film addFilm(@RequestBody Film film) {
+        int id = nextId();
+        film.setId(id);
+        films.put(id, film);
+        return film;
+    }
+
+    private int nextId() {
+        int maxId = films.values().stream()
+                .map(Film::getId)
+                .max(Integer::compareTo)
+                .orElse(0);
+        return ++maxId;
+    }
 }
