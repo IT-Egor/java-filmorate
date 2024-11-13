101bfc75eb52f2097d174b2e624f09dab22118f1
IT-Egor <egor.belousov.04@yandex.ru>
2024-11-13 18:53:02 +0300

    Add new functionality to FilmController

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.FilmService;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
 
 import java.util.Collection;
@@ -14,6 +15,7 @@ import java.util.Collection;
 @AllArgsConstructor
 public class FilmController {
     private final FilmStorage filmStorage;
+    private final FilmService filmService;
 
     @GetMapping
     public Collection<Film> getFilms() {
@@ -28,5 +30,20 @@ public class FilmController {
     @PutMapping
     public Film updateFilm(@RequestBody Film film) {
         return filmStorage.updateFilm(film);
+    }
+
+    @PutMapping("/{id}/like/{userId}")
+    public void likeFilm(@PathVariable Long id, @PathVariable Long userId) {
+        filmService.addLike(id, userId);
+    }
+
+    @DeleteMapping("/{id}/like/{userId}")
+    public void unlikeFilm(@PathVariable Long id, @PathVariable Long userId) {
+        filmService.removeLike(id, userId);
+    }
+
+    @GetMapping("/popular")
+    public Collection<Film> getMostPopularFilms(@RequestParam(required = false, defaultValue = "10", value = "count") int count) {
+        return filmService.getMostPopularFilms(count);
     }
 }
