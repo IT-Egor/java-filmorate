11a42791c605af0bae1c831708817f320f641287
IT-Egor <egor.belousov.04@yandex.ru>
2024-12-13 23:21:30 +0300

    Add data integrity exception handling

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.yandex.practicum.filmorate.exception.BadRequestException;
 import ru.yandex.practicum.filmorate.exception.InternalServerException;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
@@ -30,6 +31,12 @@ public class ErrorHandler {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleInternalServerException(InternalServerException e) {
         return new ErrorResponse(e.getMessage(), LocalDateTime.now(), 500);
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleBadRequestException(BadRequestException e) {
+        return new ErrorResponse(e.getMessage(), LocalDateTime.now(), 400);
     }
 
     @ExceptionHandler
--- src/main/java/ru/yandex/practicum/filmorate/storage/impl/db/BaseDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/impl/db/BaseDbStorage.java
@@ -1,10 +1,13 @@
 package ru.yandex.practicum.filmorate.storage.impl.db;
 
 import lombok.AllArgsConstructor;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
+import ru.yandex.practicum.filmorate.exception.BadRequestException;
 import ru.yandex.practicum.filmorate.exception.InternalServerException;
 
 import java.sql.PreparedStatement;
@@ -36,25 +39,29 @@ public abstract class BaseDbStorage<T> {
     }
 
     protected long merge(String mergeQuery, Object... params) {
-        GeneratedKeyHolder keyHolder = new GeneratedKeyHolder();
-        jdbc.update(connection -> {
-            PreparedStatement ps = connection
-                    .prepareStatement(mergeQuery, Statement.RETURN_GENERATED_KEYS);
-            for (int paramIndex = 0; paramIndex < params.length; paramIndex++) {
-                ps.setObject(paramIndex + 1, params[paramIndex]);
+        try {
+            GeneratedKeyHolder keyHolder = new GeneratedKeyHolder();
+            jdbc.update(connection -> {
+                PreparedStatement ps = connection
+                        .prepareStatement(mergeQuery, Statement.RETURN_GENERATED_KEYS);
+                for (int paramIndex = 0; paramIndex < params.length; paramIndex++) {
+                    ps.setObject(paramIndex + 1, params[paramIndex]);
+                }
+                return ps;
+            }, keyHolder);
+
+            Long id = keyHolder.getKeyAs(Long.class);
+
+            if (id != null) {
+                return id;
+            } else {
+                int methodBeginIndex = 0;
+                int methodEndIndex = 6;
+                throw new InternalServerException(String.format("Failed to %s data",
+                        mergeQuery.substring(methodBeginIndex, methodEndIndex).toLowerCase()));
             }
-            return ps;
-        }, keyHolder);
-
-        Long id = keyHolder.getKeyAs(Long.class);
-
-        if (id != null) {
-            return id;
-        } else {
-            int methodBeginIndex = 0;
-            int methodEndIndex = 6;
-            throw new InternalServerException(String.format("Failed to %s data",
-                    mergeQuery.substring(methodBeginIndex, methodEndIndex).toLowerCase()));
+        } catch (DataIntegrityViolationException e) {
+            throw new BadRequestException("Data integrity violation");
         }
     }
 }
