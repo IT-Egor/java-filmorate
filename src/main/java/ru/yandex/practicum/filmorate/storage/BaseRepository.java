7b88c428edd9c4d30123655cc2c7a11698ecbafa
Иван Соснин <i.sosnin@mafin.ru>
2025-01-17 01:50:48 +0300

    fix linter

--- src/main/java/ru/yandex/practicum/filmorate/storage/BaseRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/BaseRepository.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.storage;
 import lombok.AllArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
@@ -11,10 +10,8 @@ import ru.yandex.practicum.filmorate.exception.BadRequestException;
 import ru.yandex.practicum.filmorate.exception.InternalServerException;
 
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Collection;
-import java.util.List;
 import java.util.Optional;
 
 @AllArgsConstructor
